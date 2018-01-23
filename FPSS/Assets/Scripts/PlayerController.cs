1ab7713ecd0368bb41952aadd1f9dd97018b0307
iGNiTe <ignarmezh@gmail.com>
2018-01-23 12:18:04 +0300

    Add start project

---
+++ FPSS/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMovement))]
+public class PlayerController : MonoBehaviour { //Input commands
+
+    [SerializeField]
+    float speed = 5f;
+    [SerializeField]
+    float sensivity = 2f;
+
+    private PlayerMovement movement;
+
+    private void Start()
+    {
+        movement = GetComponent<PlayerMovement>();
+    }
+
+    private void Update()
+    {
+        //input keys from keyboard
+        float _xMov = Input.GetAxis("Horizontal"); 
+        float _zMov = Input.GetAxis("Vertical");
+
+        //vectors for movement player
+        Vector3 _movHorizontal = transform.right * _xMov;
+        Vector3 _movVertical = transform.forward * _zMov;
+        
+        Vector3 _velocity = (_movHorizontal + _movVertical).normalized * speed; //main movement vector for method Move
+
+        movement.Move(_velocity); //set movement vector
+
+        float _yRot = Input.GetAxisRaw("Mouse X"); //input mouse rotation to left and right
+
+        Vector3 _rotation = new Vector3(0f,_yRot,0f) * sensivity; //create a vector for rotate person and camera to left and right
+
+        movement.Rotate(_rotation); //set a vector for rotate person and camera to left and right
+
+        float _xRot = Input.GetAxisRaw("Mouse Y") * -1; //input mouse rotation to up and down
+
+        Vector3 _cameraRotation = new Vector3(_xRot,0f,0f) * sensivity; //create a vector for rotate ONLY camera to up and down
+
+        movement.RotateCamera(_cameraRotation);  //set a vector for rotate ONLY camera to left and right
+    }
+
+}
---
+++ FPSS/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement : MonoBehaviour {
+
+    [SerializeField]
+    private Camera cam;
+
+    private Vector3 velocity = Vector3.zero;
+    private Vector3 rotation = Vector3.zero;
+    private Vector3 rotationCamera = Vector3.zero;
+
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //Get a movement vector
+    public void Move(Vector3 _velocity)
+    {
+        velocity = _velocity;
+    }
+    public void Rotate(Vector3 _rotation)
+    {
+        rotation = _rotation;
+    }
+    public void RotateCamera(Vector3 _rotationCamera)
+    {
+        rotationCamera = _rotationCamera;
+    }
+
+    //Run every physics iteration
+    private void FixedUpdate()
+    {
+        PerformMovement();
+        PerformRotation();
+    }
+
+    void PerformMovement()
+    {
+        if(velocity != Vector3.zero)
+        {
+            rb.MovePosition(rb.transform.position + velocity * Time.fixedDeltaTime);
+        }
+    }
+
+    void PerformRotation()
+    {
+        if (rotation != Vector3.zero)
+        {
+            rb.MoveRotation(rb.rotation * Quaternion.Euler(rotation));
+        }
+        if (cam != null)
+        {
+            cam.transform.Rotate(rotationCamera);
+        }
+    }
+    
+
+}
